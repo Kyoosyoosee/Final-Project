8caae4bbb9b29fdfff65d5dddfa86b9c5415ce95
Kyoosyoosee <ShawnJiang@MacBook-Pro.local>
2015-05-04 22:15:05 -0400

    vga driver

---
+++ VGA.vhd
@@ -0,0 +1,83 @@
+-- Alex Rinker
+-- VGA Display Main Component
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity VGA is
+	port
+	(
+	 clk        : in std_logic;
+	 main_reset : in std_logic;
+	 h_sync_out : out std_logic;
+	 v_sync_out : out std_logic;
+	 d_red      : out std_logic_vector(3 downto 0);
+	 d_green    : out std_logic_vector(3 downto 0);
+	 d_blue     : out std_logic_vector(3 downto 0);
+	 L1         : in std_logic; -- Left Key
+	 R1         : in std_logic; -- Right Key
+	 U1         : in std_logic; -- Up Key
+	 D1         : in std_logic -- Down Key
+	);
+	
+	end VGA;
+
+architecture rtl of VGA is
+	
+	-- Link in the Pixel Clock
+	component altpll1 is
+		PORT
+		(
+			inclk0		: IN STD_LOGIC  := '0';
+			c0		      : OUT STD_LOGIC 
+		);
+	end component;
+	
+	-- Link in the Controller
+	component VGAController is
+	
+		port
+	   (
+		 pixel_clk : in std_logic;  -- Pixel Clock at frequency 40 MHz
+		 reset     : in std_logic;  -- Reset Button
+		 h_sync    : out std_logic; -- Horizonal Sync Signal
+		 v_sync    : out std_logic; -- Vertical Sync Signal
+		 denable   : out std_logic; -- Display enabler
+		 row       : out integer;   -- Y pixel value (0 = top row)
+		 column    : out integer   -- X pixel value (0 = leftmost column)
+	);
+	end component;
+	
+	-- Link in the Display
+	component IGF is
+		port(
+		clk            : in std_logic;
+		reset          : in std_logic;
+		enable         : in std_logic; -- Display Enable
+		row_point      : in integer; -- Row Coordinate
+		column_point	: in integer; -- Column Coordinate
+		L              : in std_logic; -- Left Key
+		R              : in std_logic; -- Right Key
+		U              : in std_logic; -- Up Key
+		D              : in std_logic; -- Down Key
+		red            : out std_logic_vector(3 downto 0);
+		green          : out std_logic_vector(3 downto 0);
+		blue           : out std_logic_vector(3 downto 0)
+	);
+	end component;
+	
+	-- Signals
+	signal pix_clk  : std_logic;
+	signal d_enable : std_logic;
+	signal row_num  : integer;
+	signal col_num  : integer;
+	
+begin
+	
+	altpll11 : altpll1 port map(inclk0 => clk, c0 => pix_clk);
+	VGAController1 : VGAController port map(pixel_clk => pix_clk, reset => main_reset, h_sync => h_sync_out, v_sync => v_sync_out, denable => d_enable, row => row_num, column => col_num);
+	IGF1 : IGF port map(clk => pix_clk, reset => main_reset, enable => d_enable, row_point => row_num, column_point => col_num, L => L1, R => R1, U => U1, D => D1, red => d_red, green => d_green, blue => d_blue);
+	
+end rtl;
+	
---
+++ VGAController.vhd
@@ -0,0 +1,117 @@
+-- Alex Rinker
+-- VGA Controller
+-- Filename: VGAController
+-- This Controller is made for use with a resolution
+-- of 800 x 600 with 60 Hz timing and a
+-- a pixel frequency of 40 MHz
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity VGAController is
+
+	-- Sets up the default values for 1600 x 1200
+	generic(
+		h_width    : integer := 1280; -- Horizontal Pixel Width
+		h_fp       : integer := 80;  -- Horizontal Front Porch
+		h_bp       : integer := 216;  -- Horizontal Back Porch
+		h_polarity : std_logic := '1'; -- Horizontal Polarity (1 = positive)
+		h_sp       : integer := 136; -- Horizontal Sync Pulse
+		
+		v_width    : integer := 960; -- Vertical Pixel Width
+		v_fp       : integer := 1;   -- Vertical Front Porch
+		v_bp       : integer := 30;  -- Vertical Back Porch
+		v_polarity : std_logic := '1'; -- Vertical Polarity (1 = positive)
+		v_sp       : integer := 3    -- Vertical Sync Pulse
+	);
+		
+	port(
+		pixel_clk : in std_logic;  -- Pixel Clock at frequency 40 MHz
+		reset     : in std_logic;  -- Reset Button
+		h_sync    : out std_logic; -- Horizonal Sync Signal
+		v_sync    : out std_logic; -- Vertical Sync Signal
+		denable   : out std_logic; -- Display enabler
+		row       : out integer;   -- Y pixel value (0 = top row)
+		column    : out integer   -- X pixel value (0 = leftmost column)
+	);
+end entity; 
+
+architecture GreekGod of VGAController is
+	
+	constant h_total : integer := h_width + h_sp + h_fp + h_bp;  -- Total number of pixels in a row
+	constant v_total : integer := v_width + v_sp + v_fp + v_bp; -- Total number of pixels in a column
+	
+	-- Create Horizontal and Vertical Counters
+	signal hpos : integer range 0 to h_total - 1 := 0; -- Horizontal Position Counter
+	signal vpos : integer range 0 to v_total - 1 := 0; -- Vertical Position Counter
+	
+begin
+	
+	Process(pixel_clk,reset)
+	begin
+		
+		-- Reset the Display
+		if reset = '0' then
+		
+			row <= 0;
+			column <= 0;
+			
+			-- Reset the Counters
+			hpos <= 0;
+			vpos <= 0;
+			
+			-- Set the Display Enable to 0
+			denable <= '0';
+			
+			-- Invert the Sync Polarities
+			h_sync <= not h_polarity;
+			v_sync <= not v_polarity;
+		
+		elsif (rising_edge(pixel_clk)) then
+			
+			-- Loop Through the Horizonal Pixels on one Row, then shift rows
+			if (hpos < h_total -1) then
+				hpos <= hpos + 1;
+			else
+				hpos <= 0; -- Reset the horizonal position
+				if (vpos < v_total -1) then -- Shift the Vertical Position and restart the loop
+					vpos <= vpos + 1;
+				else
+					vpos <= 0;
+				end if;
+			end if;
+			
+			-- Horizonal Syncing
+			if ((hpos > h_fp) and (hpos < h_bp)) then  -- check to see if the counter is betwen the display length and the back porch
+				h_sync <= not h_polarity;
+			else
+				h_sync <= h_polarity;
+			end if;
+			
+			-- Vertical Syncing
+			if ((vpos > v_fp) and (vpos < v_bp)) then  -- check to see if the counter is betwen the display length and the back porch
+				v_sync <= not v_polarity;
+			else
+				v_sync <= v_polarity;
+			end if;
+			
+			-- Set Pixel Locations
+			if (hpos > (h_fp + h_bp + h_sp) and hpos < h_total) then
+				column <= hpos - (h_fp + h_bp + h_sp);  -- set the current horizontal position to the column
+			end if;
+			
+			if (vpos > (v_fp + v_bp + v_sp) and vpos < v_total) then
+				row <= vpos - (v_fp + v_bp + v_sp);  -- set the current vertical position to the row
+			end if;
+			
+			-- Display Enable
+			if (hpos > (h_fp + h_bp + h_sp) and hpos < h_total and vpos > (v_fp + v_bp + v_sp)  and vpos < v_total) then	
+				denable <= '1';											 	--enable display
+			else																	
+				denable <= '0';												--disable display
+			end if;
+				
+		end if;
+	end process;
+end GreekGod;
